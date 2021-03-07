29f45995d262529bceab5f7422caba121ad169ae
rrraw7w <rrr.a.w7wPR@gmail.com>
2021-03-07 20:31:25 +0300

    banner

---
+++ text.py
@@ -0,0 +1,10 @@
+import random
+colors = random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;34m','\033[1;35m','\033[1;36m'])
+banner = colors+"""
+█▀▀ █▀▄▀█ █▀▀ █▀▀▄ █▀▀ █▀▀█ ▀█▀  xxxxx     
+▀▀█ █ ▀ █ ▀▀█ █▀▀▄ █▀▀ █▄▄▀  █     xxxxxx  
+▀▀▀ ▀   ▀ ▀▀▀ ▀▀▀  ▀▀▀ ▀    ▀▀▀      xxxxxx
+Telegram канал - t.me/joinchat/UecEsrd8W4Kz9o_w
+"""
+
+cursor = colors+"sms.bepi>> "
